8d1caea5c681eb232fbc6e97c1ebef28f392a281
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2025-05-26 01:49:49 +0200

    hello

--- src/main/java/net/damrongmc/dinosaurmod/item/ModItems.java
+++ src/main/java/net/damrongmc/dinosaurmod/item/ModItems.java
@@ -3,14 +3,15 @@ package net.damrongmc.dinosaurmod.item;
 import net.damrongmc.dinosaurmod.DinosaurMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item VELOCIRAPTOR_CLAW = registerItem("velociraptor_claw", new Item(new Item.Settings()));
+    public static final Item VELOCIRAPTOR_CLAW = registerItem("velociraptor_claw", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(DinosaurMod.MOD_ID,"velociraptor_claw")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(DinosaurMod.MOD_ID, name), item);
