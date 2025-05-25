8791e68a9bc9e5aac60712c0d02c5a4a4c0dc395
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2025-05-26 01:07:31 +0200

    Initial commit

---
+++ src/main/java/net/damrongmc/dinosaurmod/DinosaurModClient.java
@@ -0,0 +1,10 @@
+package net.damrongmc.dinosaurmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DinosaurModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
