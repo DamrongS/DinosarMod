2414ac02a83ea3180752ce956c3577514555f1a3
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2025-05-26 01:43:02 +0200

    hello

---
+++ src/main/java/net/damrongmc/dinosaurmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.damrongmc.dinosaurmod.item;
+
+import net.damrongmc.dinosaurmod.DinosaurMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item VELOCIRAPTOR_CLAW = registerItem("velociraptor_claw", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(DinosaurMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        DinosaurMod.LOGGER.info("Registering Mod Items for " + DinosaurMod.MOD_ID);
+
+        //Ingredients item group
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(VELOCIRAPTOR_CLAW);
+        });
+    }
+}
